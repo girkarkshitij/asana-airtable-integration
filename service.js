de322c8bea26374bfc505c09bbf9aa4434132fd4
Kshitij Girkar <girkarkshitij@gmail.com>
2023-08-22 23:32:57 +0530

    setup asana webhook

--- service.js
+++ service.js
@@ -1,6 +1,50 @@
 const express = require('express')
+const crypto = require('crypto')
 const app = express()
-const port = 3000
+const port = 8080
+
+app.use(express.json())
+
+let secret = "";
+
+app.post("/receiveWebhook", (req, res) => {
+  if (req.headers["x-hook-secret"]) {
+    console.log("This is a new webhook");
+    secret = req.headers["x-hook-secret"];
+
+    res.setHeader("X-Hook-Secret", secret);
+    res.sendStatus(200);
+  } else if (req.headers["x-hook-signature"]) {
+    const computedSignature = crypto
+      .createHmac("SHA256", secret)
+      .update(JSON.stringify(req.body))
+      .digest("hex");
+
+    if (
+      !crypto.timingSafeEqual(
+        Buffer.from(req.headers["x-hook-signature"]),
+        Buffer.from(computedSignature)
+      )
+    ) {
+      // Fail
+      res.sendStatus(401);
+    } else {
+      // Success
+      res.sendStatus(200);
+      console.log(`Events on ${Date()}:`);
+      console.log(req.body.events);
+
+      const taskAdded = req.body.events[0];
+
+      if(taskAdded){
+        console.log("*********************");
+        console.log(taskAdded);
+      }
+    }
+  } else {
+    console.error("Something went wrong!");
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Hello Express')
